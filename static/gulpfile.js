83f7f4de3d5f9b495f9949c0f747397c38c8c9f6
houfumao <essa1098@gz.essa>
2017-06-20 11:33:04 +0800

    调整css

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -1,18 +1,35 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var sourcemaps = require('gulp-sourcemaps');
+var
+  gulp = require('gulp'),
+  less = require('gulp-less'),
+  cleancss = require('gulp-clean-css'),
+  sourcemaps = require('gulp-sourcemaps'),
+  sequence = require("gulp-sequence");
 
-gulp.task('less-all', function () {
-  gulp.src(['less/app-all.less'])
+//实际只使用了压缩后的app-all.css
+gulp.task('less-app', function () {
+  return gulp.src(['less/app-all.less'])
     .pipe(less())
     .pipe(sourcemaps.write())
+    .pipe(cleancss())
     .pipe(gulp.dest('css/css-build'));
+
+});
+
+//将less文件单独生成css,不做压缩
+//这里生成的css并没有用，只是生成后方便检查css样式是否写错
+gulp.task('less-check', function () {
+  return gulp.src(['less/*.less'])
+    .pipe(less())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('css/css-check'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('less/*.less', ['less-all']); //当所有less文件发生改变时，调用testLess任务
+  gulp.watch('less/*.less', ['build']);
 });
 
-gulp.task('default', function() {
-  gulp.start('less-all');
-});
+gulp.task("build", sequence(
+  ['less-check', 'less-app']
+));
+
+gulp.task("default", ["build"], function () {});
