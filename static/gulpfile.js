9582d97d64b01d0facd4500566b7717b8f8482e7
houfumao <essa1098@gz.essa>
2017-06-20 09:51:14 +0800

    css 合并编译less

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -2,14 +2,17 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('less', function () {
+gulp.task('less-all', function () {
   gulp.src(['less/app-all.less'])
     .pipe(less())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('css/css-build'));
 });
 
+gulp.task('watch', function () {
+  gulp.watch('less/*.less', ['less-all']); //当所有less文件发生改变时，调用testLess任务
+});
 
 gulp.task('default', function() {
-  gulp.start('less');
+  gulp.start('less-all');
 });
